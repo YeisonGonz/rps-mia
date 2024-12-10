c76bf35ef3997ee052d32606615db970010e64ea
YeisonGonz <yeisonrascado@gmail.com>
2024-12-10 16:42:04 +0100

    feat(Agent.py): Cargar estado de un fichero csv

--- src/modelAgent/Agent.py
+++ src/modelAgent/Agent.py
@@ -1,11 +1,28 @@
+import csv
+import os
 from src.base_rps import GameAction
 
 
 class Agent:
 
     def __init__(self):
-        self.state = None
+        self.DEFAULT_STATE_PATH = '../data/state.csv'
+        self.state = self.load_state_by_csv()
 
     def play(self):
         if not self.state:
             return GameAction.Rock
+
+
+    def load_state_by_csv(self):
+        if not os.path.exists(self.DEFAULT_STATE_PATH):
+            return None
+
+        state = []
+
+        with open(self.DEFAULT_STATE_PATH, 'r') as file:
+            my_reader = csv.reader(file, delimiter=',')
+            for row in my_reader:
+                state.append(row)
+
+        return state
--- test/test_model.py
+++ test/test_model.py
@@ -9,3 +9,10 @@ def test_empty_histoy():
     Comprueba que la respuesta del Agente cuando no tiene informacion en el historico de partidas
     """
     assert testing_agent.play() == GameAction.Rock
+
+def test_load_state():
+    """
+    Leer bien el csv del historico de partidas
+    """
+    testing_agent.DEFAULT_STATE_PATH = '../src/data/default_state.csv' # Pequeno csv para hacer pruebas
+    assert testing_agent.load_state_by_csv() == [['ROCK','PAPER','WIN']]
